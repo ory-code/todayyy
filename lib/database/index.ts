6f02d6509d8b7fdd4cb9f14fa05bb7e00b45d6d1
ory-code <orysacha@yahoo.be>
2024-02-21 13:48:38 +0100

    fix connection mongodb

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,20 +1,21 @@
 import mongoose from 'mongoose';
 
-const MONGO_DB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || {conn: null, promise: null};
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-export const connectDB = async () => {
-    if  (cached.conn) return cached.conn;
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-    if (!MONGO_DB_URI) throw new Error("MONGODB_URI missing !")
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    cached.promised = cached.promised || mongoose.connect(MONGO_DB_URI, {
-        dbName: 'todayyy',
-        bufferCommands: false
-    })
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'todayyy',
+    bufferCommands: false,
+  })
 
-    cached.conn = await cached.promise;
+  cached.conn = await cached.promise;
+  console.log('Connected to MongoDB');
 
-    return cached.conn
+  return cached.conn;
 }
